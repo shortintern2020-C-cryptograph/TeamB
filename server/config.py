1ce7061a2bb84aa25ec2712ca6fd39e2d2243190
Daichi Azuma <daichiazuma@PC-A241.local>
2020-09-18 14:30:41 +0900

    Flask + migrate

--- server/config.py
+++ server/config.py
@@ -1,12 +1,16 @@
-class SystemConfig:
+class DevelopmentConfig:
 
   DEBUG = True
 
   SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
-      'user': 'root',
+      'user': 'admin',
       'password': 'hoge',
       'host': 'db',
       'db_name': 'hoge'
   })
 
-Config = SystemConfig
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SQLALCHEMY_ECHO = False
+
+
+Config = DevelopmentConfig
