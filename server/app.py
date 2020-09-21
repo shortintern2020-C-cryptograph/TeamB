c0c3abf8d49609e2953a15104cd76c132141699e
ThunderRa1n <Podsn0w@gmail.com>
2020-09-21 13:21:19 +0900

    update seed file for genres

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
 from flask_restful import Api
 from database import init_db
-from apis.hoge import HogeListAPI, HogeAPI
 from apis.genre import GenreListAPI, GenreAPI
 from config import Config
 
@@ -14,10 +13,8 @@ def create_app():
     init_db(app)
 
     api = Api(app)
-    api.add_resource(HogeListAPI, '/hoges')
-    api.add_resource(HogeAPI, '/hoges/<id>')
-    api.add_resource(GenreListAPI, '/genres')
-    api.add_resource(GenreAPI, '/genres/<id>')
+    api.add_resource(GenreListAPI, '/communities')
+    api.add_resource(GenreAPI, '/communities/<id>')
 
     return app
 
