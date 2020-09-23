7e35f8ad9fc2d9e059564e78c4792e66a2a5b204
koudai <61653253+koudai0619@users.noreply.github.com>
2020-09-23 14:41:33 +0900

    first commit

--- server/app.py
+++ server/app.py
@@ -7,19 +7,20 @@ from database import init_db
 from apis.hoge import HogeListAPI, HogeAPI
 from apis.genre import GenreAPI
 
+
 def create_app():
 
-  app = Flask(__name__)
-  app.config.from_object('config.Config')
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
 
-  init_db(app)
+    init_db(app)
 
-  api = Api(app)
-  api.add_resource(HogeListAPI, '/hoges')
-  api.add_resource(HogeAPI, '/hoges/<id>')
-  api.add_resource(GenreAPI, '/community/<id>')
+    api = Api(app)
+    api.add_resource(HogeListAPI, '/hoges')
+    api.add_resource(HogeAPI, '/hoges/<id>')
+    api.add_resource(GenreAPI, '/community/<id>')
 
-  return app
+    return app
 
 
 app = create_app()
